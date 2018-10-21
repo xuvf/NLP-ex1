1a3623f8166ee571e51a8f33fb6f19f7fbb6ccb2
wxx522 <wxx522@cs.bham.ac.uk>
2018-10-22 00:17:27 +0100

    Calculate tkoens and V size

--- NLP-ex1.py
+++ NLP-ex1.py
@@ -1,26 +1,47 @@
 import json
 import nltk
 import re
+import datetime
 
-f = open("1.json","r")
+#Start Timer
+start_time = datetime.datetime.now()
+
+f = open("signal-news1.jsonl","r")
 
 data = []
 
 for line in f :
-    data.append(json.loads(line).get("content"))
+    temp = json.loads(line).get("content")
+    
+    # Lower the characters
+    temp = temp.lower()
+    
+    
+    #Q4 remove all URLs
+    rule = re.compile(r"http(s)?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]")
+    temp = rule.sub('', temp)
+    
+    #Q1 remove all non-alphacharacters except spaces
+    rule = re.compile("[^a-z0-9 ]")
+    temp = rule.sub('',temp)
+    
+    #Q2 remove all words within 3 characters
+    rule = re.compile(r"\b\w{1,3}\b")
+    temp = rule.sub('', temp)
+    
+    #Q3 remove numbers that are fully made of digits
+    rule = re.compile(r"([0-9]+ )")
+    temp = rule.sub('', temp)
+    
+    temp = nltk.word_tokenize(temp)
+    
+    temp = nltk.pos_tag(temp)
+    
+    data.append(temp)
 
-# Lower the characters
-data[0] = data[0].lower()
+end_time = datetime.datetime.now()
+interval = (end_time-start_time).seconds
 
+print (interval)
 
-#Q1 remove all non-alphacharacters except spaces
-rule = re.compile("[^a-z ]")
-data[0] = rule.sub('',data[0])
-
-#Q2 remove all words within 3 characters
-rule = re.compile(r"\b\w{1,3}\b")
-data[0] = rule.sub('', data[0])
-
-#Q3 remove numbers that are fully made of digits
-#rule = re.compile(r"")
-print(data[0])
+print(data[6])
